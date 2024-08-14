d4c57ef0003bd030acfa6510f71f593d18d99ce8
Muhammad Amsal <muhammadamsal526@gmail.com>
2024-08-14 15:36:19 +0500

    navbar com

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+ import Navbar from './components/Navbar'
+import { UpdateFollower } from 'react-mouse-follower'
+
+function App() {
+   
+
+  return (
+    <main className='overflow-x-hidden' >
+    <UpdateFollower
+    mouseOptions={{
+      backgroundColor:"white",
+      zIndex:999,
+      followSpeed:1.5
+    }}
+    >
+
+      <Navbar/>
+    </UpdateFollower>
+    </main>
+  )
+}
+
+export default App
